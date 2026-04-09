7cda381e93ab438c6975c7f6792a95ff3d469881
mineot <mineot@gmail.com>
2026-04-08 22:20:55 -0300

    :sparkles: feat/essential: tsup configuration

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: {
+    fortuna: "src/main.ts",
+  },
+  format: ["esm"],
+  outDir: "dist",
+  bundle: true,
+  splitting: false,
+  sourcemap: false,
+  minify: true,
+  clean: true,
+  treeshake: true,
+});
