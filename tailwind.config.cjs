3b4b253ef6aa4fa22d811b22bfe5bd3e2f2effe8
mineot <mineot@gmail.com>
2026-04-12 18:02:22 -0300

    :sparkles: feat/styles: upgrade tailwindcss to v4 and create theme css

--- tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/renderer/index.html', './src/renderer/src/**/*.{ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
