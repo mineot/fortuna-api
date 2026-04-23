fd4df9c94c8b36cec6e03e9e9123eac8951a4977
mineot <mineot@gmail.com>
2026-04-23 19:40:34 -0300

    :recycle: refactor/styles: improvement styles

--- src/renderer/src/components/app-shell.tsx
+++ src/renderer/src/components/app-shell.tsx
@@ -22,50 +22,23 @@ export function AppShell({ children }: PropsWithChildren) {
 
   return (
     <div className="flex flex-col gap-2 p-2 overflow-auto">
-      <div>
-        <button>Button Normal</button>
-      </div>
-      <div>
-        <button className="flat">Button Flat</button>
-      </div>
-      <div className="input-group">
-        <label htmlFor="test-input">Label</label>
-        <input id="test-input" type="text" placeholder="Text Input" />
-      </div>
-      <div className="input-group linear">
-        <label htmlFor="test-input-linear">Label</label>
-        <input id="test-input-linear" type="text" placeholder="Text Input Linear" />
-      </div>
-      <div className="input-group checkbox">
-        <input id="test-input-checkbox" type="checkbox" placeholder="checkbox" />
-        <label htmlFor="test-input-checkbox">Label</label>
-      </div>
-      <div className="input-group radio">
-        <input id="test-input-radio" type="radio" placeholder="radio" name="radio-text" value="1" />
-        <label htmlFor="test-input-radio">Label</label>
-      </div>
-      <div className="input-group radio">
-        <input id="test-input-radio-2" type="radio" placeholder="radio" name="radio-text" value="2" />
-        <label htmlFor="test-input-radio-2">Label</label>
-      </div>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Column 1</th>
-              <th>Column 2</th>
-              <th>Column 3</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Cell 1</td>
-              <td>Cell 1</td>
-              <td>Cell 1</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Cell 1</td>
+            <td>Cell 1</td>
+            <td>Cell 1</td>
+          </tr>
+        </tbody>
+      </table>
+      {/*
       <div>
         <button>Open Modal</button>
         <div className="modal">
@@ -75,7 +48,7 @@ export function AppShell({ children }: PropsWithChildren) {
             <div className="footer">Footer</div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
